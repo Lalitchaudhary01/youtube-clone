51a2c1a34516f31868c1de9cdc1e715a563e1dd6
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-02-15 23:44:35 +0530

    add icons

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -23,21 +23,23 @@ const VideoCard = ({ info }) => {
   }
 
   return (
-    <div className="mx-4 my-2 w-64 rounded overflow-hidden shadow-lg bg-gray-800 text-white">
-      <img
-        className="w-full"
-        src={thumbnails.medium.url}
-        alt="Video Thumbnail"
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-gray-400 text-base">{channelTitle}</p>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-600 rounded-full px-3 py-1 text-sm font-semibold mr-2">
-          {statistics.viewCount} views
-        </span>
-        {/* Additional statistics could be added here */}
+    <div className="my-3 mx-5 w-60 shadow-lg">
+      <div className=" w-64 h-70 rounded overflow-hidden shadow-lg bg-gray-800 text-white">
+        <img
+          className="w-30 h-30 object-cover"
+          src={thumbnails.medium.url}
+          alt="Video Thumbnail"
+        />
+        <div className="px-3 py-1">
+          <div className=" text-lg h-12 mb-3 overflow-hidden">{title}</div>
+          <p className="text-gray-400 mb-1 text-base h-8 overflow-hidden">
+            {channelTitle}
+          </p>
+          <div className="flex justify-between text-gray-400 text-sm mt-1">
+            <span>{statistics.viewCount} views</span>
+            {/* Additional statistics could be added here */}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSLice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
-  return <div>watch live</div>;
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
+
+  const dispatch = useDispatch();
+  dispatch(() => {
+    dispatch(closeMenu());
+  }, []);
+
+  const videoUrl = `https://www.youtube.com/embed/${searchParams.get("v")}`;
+
+  return (
+    <div>
+      <iframe
+        className="px-5 py-2"
+        width="1000"
+        height="500"
+        src={videoUrl }
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchPage;
