f06a23dac37be11521fda8e67e6391dbabdb98a3
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-02-17 23:58:32 +0530

    add comment section

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSLice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -15,17 +16,20 @@ const WatchPage = () => {
   const videoUrl = `https://www.youtube.com/embed/${searchParams.get("v")}`;
 
   return (
-    <div>
-      <iframe
-        className="px-5 py-2"
-        width="1000"
-        height="500"
-        src={videoUrl }
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+    <div className="flex flex-col">
+      <div>
+        <iframe
+          className="px-5 py-2"
+          width="1000"
+          height="500"
+          src={videoUrl}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <CommentsContainer />
     </div>
   );
 };
