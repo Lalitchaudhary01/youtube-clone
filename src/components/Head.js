acebf7501959dff4e4d1e44bb3755478b2a53608
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-02-17 22:39:58 +0530

    update readme and build well optimise search

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,65 +1,102 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSLice";
-import { useDispatch } from "react-redux";
 import { YOUTUBE_SEARCH_API } from "../utils/contants";
+import { cacheResults } from "../utils/SearchSlice";
 
 const Head = () => {
+  // useState hooks
   const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
-  useEffect(() => {
-    const timer = setTimeout(() => getSearchSuggestions(), 200);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [searchQuery]);
-  const getSearchSuggestions = async () => {
-    console.log("API CALL -" + searchQuery);
-    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
-    const json = await data.json();
-  };
+  const searchCache = useSelector((store) => store.search);
 
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions();
+      }
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  // #About Debouncing
+  // key stroke - i
+  // - render the Component
+  // - useEffect()
+  // - start timer -> make api call after 200ms
+
+  // key stroke - ip
+  // - destroy the component (useEffect return method) effective if and only if p key is pressed before the 200ms timer expired or else it will try to destroy the timer which is already expired with an api call made
+  // - re-render the component
+  // - useEffect
+  // - start timer -> make api call after 200ms
+
+  const getSearchSuggestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+
+    setSuggestions(json[1]);
+
+    // update cache
+    dispatch(
+      cacheResults({
+        [searchQuery]: json[1],
+      })
+    );
+  };
+
   return (
     <div className="grid grid-cols-3 items-center p-4 bg-gray-900 shadow-md">
       <div className="flex items-center">
         <img
-          onClick={() => toggleMenuHandler()}
+          onClick={toggleMenuHandler} // Simplified onClick event
           className="cursor-pointer h-8 w-auto mr-2 ml-4 mx-5"
           alt="menu"
           src="https://www.shutterstock.com/image-vector/hamburger-menu-icon-mobile-apps-260nw-360130484.jpg"
         />
         <img
-          className="h-10 w-auto mx-12 "
+          className="h-10 w-auto mx-12"
           alt="youtube-logo"
           src="https://ongpng.com/wp-content/uploads/2023/04/3-13.png"
         />
       </div>
-      <div className="flex ">
+      <div className="flex relative">
         <input
           className="px-4 py-2 w-full rounded-md bg-gray-800 text-white focus:outline-none"
           type="text"
           placeholder="Search"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
+          onFocus={() => setShowSuggestions(true)}
+          onBlur={() => setShowSuggestions(false)}
         />
         <button className="ml-2 px-4 py-2 bg-red-600 rounded-md text-white">
           Search
         </button>
-        <div className="fixed bg-white my-10 shadow-lg">
-          <ul>
-            <li className="py-2 shadow-lg px-2">I phone</li>
-            <li>I phone</li>
-            <li>I phone</li>
-            <li>I phone</li>
-            <li>I phone</li>
-          </ul>
-        </div>
+        {showSuggestions && suggestions.length > 0 && (
+          <div className="absolute bg-white py-2 px-2 my-11 shadow-lg w-[22rem] rounded-md">
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="px-3 py-2 shadow-sm hover:bg-gray-100">
+                  {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
-
       <div className="flex justify-end">
         <img
           className="h-8 w-auto"
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSLice from "./appSLice";
+import SearchSlice from "./SearchSlice";
 
 const store = configureStore({
   reducer: {
     app: appSLice,
+    search: SearchSlice,
   },
 });
 
