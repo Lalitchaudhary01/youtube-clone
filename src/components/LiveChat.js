dcf60ec8aa4dc115d7d8c7ed6506fe633cf6a5a2
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-02-19 23:43:00 +0530

    build live chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { generateRandomName } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
+  const dispatch = useDispatch();
+
+  const chatMessage = useSelector((store) => store.chat.message);
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      //api polling
+      // console.log("Api");
+      dispatch(
+        addMessage({
+          name: generateRandomName(),
+          message: "hey!!",
+        })
+      );
+    }, 100);
+    return () => clearInterval(i);
+  }, []);
+
   return (
-    <div className="w-1310 h-[485px]   border border-black">
-      <ChatMessage name="Lalit chaudhary" message="hey buddy!!!" />
-    </div>
+    <>
+      <div className="w-1310 h-[485px] border border-black overflow-y-scroll flex flex-col-reverse">
+        <div>
+          {chatMessage.map((c, i) => (
+            <ChatMessage key={i} name={c.name} message={c.message} />
+          ))}
+        </div>
+      </div>
+      <form
+        className="w-full border border-black flex"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("on form submit", liveMessage);
+        }}
+      >
+        <input
+          className="w-full px-4 py-2 outline-none"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+        />
+        <button className="px-4 m-2 py-2 bg-gray-400 text-white font-semibold rounded-r">
+          Submit
+        </button>
+      </form>
+    </>
   );
 };
 
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appSLice from "./appSLice";
 import SearchSlice from "./SearchSlice";
+import chatSlice from "./chatSlice";
 
 const store = configureStore({
   reducer: {
     app: appSLice,
     search: SearchSlice,
+    chat: chatSlice,
   },
 });
 
